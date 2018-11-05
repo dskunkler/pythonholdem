bda93308f005ad3ede1e89a8bd2882a82f8b39c4
Paul <phunter510@gmail.com>
2018-11-04 21:06:07 -0600

    straight added

--- hand_values.py
+++ hand_values.py
@@ -13,25 +13,30 @@
 def get_winner(player_list, comm_cards):
     []
 
-# Determines player's hand
+# Determines player's hand values
 def hand_value(hand):
-    hand.sort(key = lambda card: card.suit)
-    hand.sort(key = lambda card: card.rank)
-    return []
-    
-def straight(hand):
-    '''
-    straight_len = 0
-    ranks = [card.rank for card in hand] # Get only ranks
-    ranks = list(set(seq)) # Creates unique list of ranks
-    
-    # Middle two comparisons must be in order for straight to exist
-    if (ranks[2]-ranks[3]==1 and ranks[3]-ranks[4]==1):
-    '''
     return []
 
+# Returns high card rank if straight exists
+# Returns -1 if no straight exists
+def straight(hand):
+    ranks = [card.rank for card in hand] # List of only ranks
+    ranks = list(set(ranks)) # Creates unique list of ranks
+    ranks.sort(reverse=True) # Sorts in descending order
+
+    # Checking sets of 5, maximum of 3 sets of 5
+    for i in range(len(ranks)-4):
+        # Spliced list of 5
+        sub_seq = ranks[i:i+5]
+        # List of [max, max-1, max-2, max-3, ..., min]
+        straight_seq = list(range(max(sub_seq),min(sub_seq)-1,-1))
+        if (sub_seq == straight_seq):
+            return ranks[i]
+        
+    return -1
+
 def flush(hand):
-    suits = [card.suit for card in hand]
+    suits = [card.suit for card in hand] # List of only suits
     suits.sort()
     suit_len = 1
     for i in range(1, len(suits)):
@@ -67,7 +72,7 @@ print(combined[0].rank)
 combined.sort(key=lambda card: card.rank, reverse=True)
 print(combined)
 
-print("\nFLUSH TEST1--------------------------------")
+print("\nFLUSH TESTS--------------------------------")
 test_1 = [Card(0,0),Card(0,1),Card(4,0),Card(10,0),
           Card(12,0),Card(5,3),Card(10,0)]
 test_2 = [Card(0,0),Card(0,1),Card(4,1),Card(10,0),
@@ -76,3 +81,9 @@ print(test_1)
 print(flush(test_1))
 print(test_2)
 print(flush(test_2))
+
+print("\nSTRAIGHT TESTS--------------------------------")
+test_3 = [Card(0,0),Card(1,1),Card(2,0),Card(3,0),
+          Card(5,3),Card(4,3),Card(10,0)]
+print(test_3)
+print(straight(test_3))
